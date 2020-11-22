00afdbe91d5badef3450c178849c1c731a07e541
Amal Joe R S <amaljoe88@gmail.com>
2020-11-22 08:49:07 +0530

    add dice image

--- app/src/main/java/com/amaljoe/diceroller/MainActivity.kt
+++ app/src/main/java/com/amaljoe/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.amaljoe.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 /**
 * This activity displays the result on the screen
@@ -11,13 +11,13 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     // Create an object for dice
     private var dice: Dice = Dice(6)
-    private lateinit var result: TextView
+    private lateinit var result: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        result = findViewById(R.id.textView)
+        result = findViewById(R.id.imageView)
         rollButton.setOnClickListener {
             rollDice()
         }
@@ -25,7 +25,17 @@ class MainActivity : AppCompatActivity() {
 
     // Function which rolls the dice
     private fun rollDice() {
-        result.text = dice.roll().toString()
+        val num = dice.roll()
+        val diceImage = when (num) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        result.setImageResource(diceImage)
+        result.contentDescription = num.toString()
     }
 }
 
